df45932f459349eb96a6ba14263fa2f39f56676c
vasanthN2001 <vasanthnachimuthu@gmail.com>
2023-06-26 13:53:41 +0530

    fragment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import LifecycleA from './components/LifecycleA';
+import Fragmentdemo from './components/Fragmentdemo';
 
 
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <LifecycleA />
+        <Fragmentdemo />
       </div>
     )
   }
---
+++ src/components/Fragmentdemo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Fragmentdemo() {
+    return (
+        <React.Fragment>
+            <h1>
+                Fragment Demo
+            </h1>
+            <p>this discribes Fragment demo Component</p>
+        </React.Fragment>
+    )
+}
+
+export default Fragmentdemo
